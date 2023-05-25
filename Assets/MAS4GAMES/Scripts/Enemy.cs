d2f1d6eb2a74f43eb83aa2447c67ebcd97167298
svillarruelv <sergiovillarruelvasquez@gmail.com>
2023-05-24 22:25:53 -0500

    Animation to Ragdoll

--- Assets/MAS4GAMES/Scripts/Enemy.cs
+++ Assets/MAS4GAMES/Scripts/Enemy.cs
@@ -4,39 +4,83 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    private enum EnemyState{
+        Walking,
+        Ragdoll
+    }
+
+    [SerializeField]
+    private Camera _camera;
+    
     private Rigidbody[] _ragdollRigidbodies;
-    private bool isRagdoll = true;
+    private EnemyState _currentState = EnemyState.Walking;
+    private Animator _animator;
+    private CharacterController _characterController;
     
     void Awake()
     {
         //Get every ragdoll component in the model
         _ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+        DisableRagdoll();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            if(isRagdoll){
-                DisableRagdoll();
-            }else{
-                EnableRagdoll();
-            }
-            isRagdoll = !isRagdoll;
+        switch(_currentState){
+            case EnemyState.Walking:
+                WalkingBehaviour();
+                break;
+            case EnemyState.Ragdoll:
+                RagdollBehaviour();
+                break;
         }
     }
 
-    private void DisableRagdoll(){
+    private void DisableRagdoll()
+    {
         foreach (var rigidBody in _ragdollRigidbodies){
-            //Stop physics affecting the rigidbody
             rigidBody.isKinematic = true;
         }
+
+        _animator.enabled = true;
+        _characterController.enabled = true;
     }
 
-    private void EnableRagdoll(){
+    private void EnableRagdoll()
+    {
         foreach (var rigidBody in _ragdollRigidbodies){
-            //Stop physics affecting the rigidbody
             rigidBody.isKinematic = false;
+        }
+
+        _animator.enabled = false;
+        _characterController.enabled = false;
+    }
+
+    private void WalkingBehaviour()
+    {
+        Vector3 direction = _camera.transform.position - transform.position;
+        direction.y = 0;
+        direction.Normalize();
+
+        Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 100 * Time.deltaTime);
+
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            EnableRagdoll();
+            _currentState = EnemyState.Ragdoll;
+        }
+    }
+
+    private void RagdollBehaviour()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            DisableRagdoll();
+            _currentState = EnemyState.Walking;
         }
     }
 }
