100d95f125417b696bdb53facea8744b6a3c59db
svillarruelv <sergiovillarruelvasquez@gmail.com>
2023-05-24 20:38:39 -0500

    switch ragdoll

--- Assets/MAS4GAMES/Scripts/Enemy.cs
+++ Assets/MAS4GAMES/Scripts/Enemy.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody[] _ragdollRigidbodies;
+    private bool isRagdoll = true;
+    
+    void Awake()
     {
-        
+        //Get every ragdoll component in the model
+        _ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(isRagdoll){
+                DisableRagdoll();
+            }else{
+                EnableRagdoll();
+            }
+            isRagdoll = !isRagdoll;
+        }
+    }
+
+    private void DisableRagdoll(){
+        foreach (var rigidBody in _ragdollRigidbodies){
+            //Stop physics affecting the rigidbody
+            rigidBody.isKinematic = true;
+        }
+    }
+
+    private void EnableRagdoll(){
+        foreach (var rigidBody in _ragdollRigidbodies){
+            //Stop physics affecting the rigidbody
+            rigidBody.isKinematic = false;
+        }
     }
 }
