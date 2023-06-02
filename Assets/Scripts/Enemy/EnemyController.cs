2b2c667414052cf2c9d815d078198927c85e38f9
svillarruelv <sergiovillarruelvasquez@gmail.com>
2023-06-01 22:54:01 -0500

    Better chasing and attack range

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -5,90 +5,87 @@ using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
-    public StatsData enemyStats = new StatsData();
+  public StatsData enemyStats = new StatsData();
 
-    private Animator enemyAnimator;
+  private Animator enemyAnimator;
 
-    [SerializeField]
-    private GameObject weapon;
-    private float range;
-    private int damage;
+  [SerializeField]
+  private GameObject weapon;
+  private float chasingRange = 8f;
+  private float attackRange;
+  private int damage;
 
-    public GameObject player;
-    private bool isAttacking = false;
+  public GameObject player;
+  private bool isAttacking = false;
 
-    [SerializeField]
-    private Slider healthBar;
+  [SerializeField]
+  private Slider healthBar;
 
-    void Start()
+  void Start()
+  {
+    enemyAnimator = GetComponent<Animator>();
+
+    //small values for eisier testing
+    enemyStats.health = 100;
+    damage = 10;
+
+    healthBar.maxValue = enemyStats.health;
+    healthBar.value = enemyStats.health;
+
+    attackRange = weapon.GetComponent<WeaponController>().weaponData.range;
+    //damage = weapon.GetComponent<WeaponController>().weaponData.damage;
+  }
+
+  void Update()
+  {
+    if (enemyStats.health <= 0) return;
+
+    healthBar.gameObject.SetActive(true);
+    healthBar.transform.LookAt(new Vector3(player.transform.position.x, healthBar.transform.position.y, player.transform.position.z));
+
+    if (Vector3.Distance(transform.position, player.transform.position) > attackRange && Vector3.Distance(transform.position, player.transform.position) < chasingRange)
     {
-        enemyAnimator = GetComponent<Animator>();
-
-        //small values for eisier testing
-        enemyStats.health = 100;
-        damage = 10;
-
-        healthBar.maxValue = enemyStats.health;
-        healthBar.value = enemyStats.health;
-
-        range = weapon.GetComponent<WeaponController>().weaponData.range;
-        //damage = weapon.GetComponent<WeaponController>().weaponData.damage;
+      //BUSCA AL JUGADOR
+      transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+      transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), 0.003f);
     }
-
-    void Update()
+    else if (!isAttacking && Vector3.Distance(transform.position, player.transform.position) <= attackRange)
     {
-        if (GetComponent<EnemyViewController>().isSeeing && player)
-        {
-            healthBar.gameObject.SetActive(true);
-            healthBar.transform.LookAt(new Vector3(player.transform.position.x, healthBar.transform.position.y, player.transform.position.z));
-
-            if (Vector3.Distance(transform.position, player.transform.position) > range)
-            {
-                transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
-                transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), 0.003f);
-            }
-            else if (!isAttacking)
-            {
-                Attack();
-            }
-        }
-        else
-        {
-            healthBar.gameObject.SetActive(false);
-        }
+      Attack();
     }
+  }
 
-    private void Attack()
+  private void Attack()
+  {
+    var playerToHit = player;
+
+    isAttacking = true;
+    enemyAnimator.SetBool("isAttacking", true);
+
+    StartCoroutine(Utility.TimedEvent(() =>
     {
-        var playerToHit = player;
+      playerToHit.GetComponent<CombatController>().TakeDamage(damage);
+    }, 1f));
 
-        isAttacking = true;
-        enemyAnimator.SetBool("isAttacking", true);
-
-        StartCoroutine(Utility.TimedEvent(() =>
-        {
-            playerToHit.GetComponent<CombatController>().TakeDamage(damage);
-        }, 1f));
-
-        StartCoroutine(Utility.TimedEvent(() =>
-        {
-            isAttacking = false;
-        }, 2.5f));
-    }
-
-    public void TakeDamage(int damage)
+    StartCoroutine(Utility.TimedEvent(() =>
     {
-        healthBar.value -= damage;
-        enemyStats.health -= damage;
-        enemyAnimator.SetBool("isHit", true);
+      isAttacking = false;
+    }, 2.5f));
+  }
 
-        if (enemyStats.health <= 0)
-        {
-            GetComponent<Collider>().enabled = false;
-            enemyAnimator.SetBool("isDead", true);
-            player = null;
+  public void TakeDamage(int damage)
+  {
+    healthBar.value -= damage;
+    enemyStats.health -= damage;
+    enemyAnimator.SetBool("isHit", true);
 
-            Destroy(gameObject, 4f);
-        }
+    if (enemyStats.health <= 0)
+    {
+      GetComponent<Collider>().enabled = false;
+      enemyAnimator.SetBool("isDead", true);
+      player = null;
+
+      Destroy(gameObject, 4f);
     }
+  }
 }
