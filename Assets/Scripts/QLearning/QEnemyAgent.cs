998c227cdb3b2f34949b91d6fb088e5a11f318c5
svillarruelv <sergiovillarruelvasquez@gmail.com>
2023-06-19 20:07:11 -0500

    qlearning finished

--- Assets/Scripts/QLearning/QEnemyAgent.cs
+++ Assets/Scripts/QLearning/QEnemyAgent.cs
@@ -5,6 +5,7 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using MyEnums;
+
 public class QEnemyAgent : Agent
 {
   private float episodeTime;
@@ -12,14 +13,13 @@ public class QEnemyAgent : Agent
   private EnemyController enemyController;
   private CombatController playerCombatController;
 
-  private float[][] QTable; //QTable from the Agent
-                            //QTable: filas (i) = estado, columnas (j) = acción
-  private float learningRate = 0.1f; //Learning Rate
-  private float discountFactor = 0.99f; //Discount Factor
+  private float[][] QTable; // QTable from the Agent
+                            // QTable: filas (i) = estado, columnas (j) = acción
+  private float learningRate = 0.1f; // Learning Rate
+  private float discountFactor = 0.99f; // Discount Factor
   private int state;
   private int action;
   float rewardPerEpisode = 0f;
-
 
   public override void Initialize()
   {
@@ -28,7 +28,7 @@ public class QEnemyAgent : Agent
 
     // Inicializar la QTable con valores aleatorios
     int numStates = 2; // Número de estados
-    int numActions = 2; // Número de acciones (moverse, atacar)
+    int numActions = 3; // Número de acciones
     QTable = new float[numStates][];
     for (int i = 0; i < numStates; i++)
     {
@@ -42,7 +42,6 @@ public class QEnemyAgent : Agent
 
   public override void OnEpisodeBegin()
   {
-
     // RESET ENEMY ATTRS
     enemyController.transform.position = new Vector3(0f, 0f, 15f);
     enemyController.enemyStats.health = 100;
@@ -57,7 +56,7 @@ public class QEnemyAgent : Agent
     playerCombatController.isTraining = true;
 
     // RESET LOGIC
-    Debug.Log($"rewardPerEpisode -> {rewardPerEpisode}");
+    Debug.Log($"AgentId {enemyController.enemyStats.id} rewardPerEpisode -> {rewardPerEpisode}");
     rewardPerEpisode = 0f;
     episodeTime = 0f;
   }
@@ -87,6 +86,7 @@ public class QEnemyAgent : Agent
     // 5. Posición del jugador
     sensor.AddObservation(playerPosition);
   }
+
   public override void OnActionReceived(ActionBuffers actions)
   {
     //Tiempo del episodio
@@ -100,63 +100,7 @@ public class QEnemyAgent : Agent
     int attackRange = actions.DiscreteActions[1]; //5
     int chasingRange = actions.DiscreteActions[2]; //5
     int speedRange = actions.DiscreteActions[3]; //5
-
-    switch (attackRange)
-    {
-      case 0:
-        enemyController.attackRange = 1f;
-        break;
-      case 1:
-        enemyController.attackRange = 1.5f;
-        break;
-      case 2:
-        enemyController.attackRange = 2f;
-        break;
-      case 3:
-        enemyController.attackRange = 2.5f;
-        break;
-      case 4:
-        enemyController.attackRange = 3f;
-        break;
-    }
-
-    switch (chasingRange)
-    {
-      case 0:
-        enemyController.chasingRange = 6f;
-        break;
-      case 1:
-        enemyController.chasingRange = 8f;
-        break;
-      case 2:
-        enemyController.chasingRange = 10f;
-        break;
-      case 3:
-        enemyController.chasingRange = 12f;
-        break;
-      case 4:
-        enemyController.chasingRange = 14f;
-        break;
-    }
-
-    switch (speedRange)
-    {
-      case 0:
-        enemyController.speedRange = 0.006f;
-        break;
-      case 1:
-        enemyController.speedRange = 0.008f;
-        break;
-      case 2:
-        enemyController.speedRange = 0.009f;
-        break;
-      case 3:
-        enemyController.speedRange = 0.011f;
-        break;
-      case 4:
-        enemyController.speedRange = 0.013f;
-        break;
-    }
+    int action = actions.DiscreteActions[3]; // Action: state, attackRange, chasingRange, speedRange
 
     if (state == 0)
     {
@@ -172,16 +116,30 @@ public class QEnemyAgent : Agent
       enemyController.Attack();
     }
 
+    switch (action)
+    {
+      case 0:
+        ModifyAgentCharacteristics(attackRange, -1, -1);
+        break;
+      case 1:
+        ModifyAgentCharacteristics(-1, chasingRange, -1);
+        break;
+      case 2:
+        ModifyAgentCharacteristics(-1, -1, speedRange);
+        break;
+    }
+
     // Actualizar el estado y la acción previa
     int previousState = state;
     int previousAction = action;
-    state = CalculateState();
 
     // Calcular la recompensa en base al cambio de estado
     float reward = CalculateReward();
-    rewardPerEpisode += reward;
     // Actualizar la QTable
     QTable[previousState][previousAction] += learningRate * (reward + discountFactor * GetMaxQValue(state) - QTable[previousState][previousAction]);
+    reward += GetMaxQValue(state);
+    rewardPerEpisode += reward;
+    SetReward(reward);
   }
 
   private bool CheckMountainCollision()
@@ -195,25 +153,6 @@ public class QEnemyAgent : Agent
       }
     }
     return false;
-  }
-
-  private bool CheckPlayerCollision()
-  {
-    Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
-    foreach (Collider collider in colliders)
-    {
-      if (collider.CompareTag("Player"))
-      {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private int CalculateState()
-  {
-    if (enemyController.isAttacking) return 1;
-    else return 0;
   }
 
   private float CalculateReward()
@@ -246,10 +185,10 @@ public class QEnemyAgent : Agent
       EndEpisode();
     }
 
-    //SI EL JUGADOR SE MUERE
+    // SI EL JUGADOR SE MUERE
     if (playerCombatController.playerStats.health <= 0)
     {
-      //SI EL JUGADOR SE MUERE MUY RAPIDO
+      // SI EL JUGADOR SE MUERE MUY RÁPIDO
       if (episodeTime <= 30f)
       {
         reward += -10f;
@@ -270,10 +209,10 @@ public class QEnemyAgent : Agent
       EndEpisode();
     }
 
-    //Enemy's health rewards
+    // Enemy's health rewards
     if (enemyController.enemyStats.health <= 0)
     {
-      //IF the enemy died too fast
+      // IF the enemy died too fast
       if (episodeTime <= 30f)
       {
         reward += -10f;
@@ -285,7 +224,7 @@ public class QEnemyAgent : Agent
       EndEpisode();
     }
 
-    //Attack rewards
+    // Attack rewards
     switch (enemyController._attackState)
     {
       case AttackStates.SUCCESS:
@@ -312,11 +251,74 @@ public class QEnemyAgent : Agent
     return reward;
   }
 
+  private void ModifyAgentCharacteristics(int attackRange, int chasingRange, int speedRange)
+  {
+    // Configurar el rango de ataque del enemigo
+    switch (attackRange)
+    {
+      case 0:
+        enemyController.attackRange = 1f;
+        break;
+      case 1:
+        enemyController.attackRange = 1.5f;
+        break;
+      case 2:
+        enemyController.attackRange = 2f;
+        break;
+      case 3:
+        enemyController.attackRange = 2.5f;
+        break;
+      case 4:
+        enemyController.attackRange = 3f;
+        break;
+    }
+
+    // Configurar el rango de persecución del enemigo
+    switch (chasingRange)
+    {
+      case 0:
+        enemyController.chasingRange = 6f;
+        break;
+      case 1:
+        enemyController.chasingRange = 8f;
+        break;
+      case 2:
+        enemyController.chasingRange = 10f;
+        break;
+      case 3:
+        enemyController.chasingRange = 12f;
+        break;
+      case 4:
+        enemyController.chasingRange = 14f;
+        break;
+    }
+
+    // Configurar la velocidad del enemigo
+    switch (speedRange)
+    {
+      case 0:
+        enemyController.speedRange = 0.006f;
+        break;
+      case 1:
+        enemyController.speedRange = 0.008f;
+        break;
+      case 2:
+        enemyController.speedRange = 0.009f;
+        break;
+      case 3:
+        enemyController.speedRange = 0.011f;
+        break;
+      case 4:
+        enemyController.speedRange = 0.013f;
+        break;
+    }
+  }
+
+
   private float GetMaxQValue(int state)
   {
     // Obtener el valor máximo de la QTable para el estado dado
     float maxQValue = Mathf.Max(QTable[state]);
-
     return maxQValue;
   }
 
